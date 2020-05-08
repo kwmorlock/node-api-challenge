fd4d5666303675015d474d8d4c92df304d58195d
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-08 11:52:39 -0400

    server.use created in server.js

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const actionRouter = require('./helpers/actionRouter');
 const projectRouter = require('./helpers/projectRouter');
 
 const server = express();
+
+server.use(express.json())
+server.use(logger);
+
+server.use("/api/action", actionRouter );
+server.use("/api/project", projectRouter);
