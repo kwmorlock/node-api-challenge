b4f1450ea7494ce22566cdaaaf12af581b43ca6a
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-08 11:53:34 -0400

    server.get setup in server

--- server.js
+++ server.js
@@ -10,3 +10,7 @@ server.use(logger);
 
 server.use("/api/action", actionRouter );
 server.use("/api/project", projectRouter);
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Let's write some functioning middleware this time!</h2>`);
+  });
